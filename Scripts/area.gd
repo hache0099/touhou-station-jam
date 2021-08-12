62e5ebc32ae8955dca8e3411981a88b46a6f9479
hache0099 <81655011+hache0099@users.noreply.github.com>
2021-08-12 01:05:44 -0300

    avance con el tira frutas

--- Scripts/area.gd
+++ Scripts/area.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+var number : int
+
+var colors = ["red", "green", "blue", "blueviolet", "orange", "yellow", "black"]
+
 var min_angle : float = 25.0
 var max_angle : float = 45.0
 
@@ -8,28 +12,74 @@ var max_force : float = 700.0
 
 var start_position : Vector2 setget set_start_position
 
+var click_rect : Rect2
+
 var angle : float
 var force : float
 
 var from_left : bool = true
 
 func _ready():
-	yield(get_tree().create_timer(0.5),"timeout")
-	launch()
+	$click_detecter.set_as_toplevel(true)
+	$Button.set_as_toplevel(true)
+	click_rect.size = Vector2(36*2,38*2)
+
+func _physics_process(delta):
+	click_rect.position = position - Vector2(36,38)
+	click()
+	$Label.set_text(str(number))
+	$Sprite.modulate = ColorN(colors[number])
+	$click_detecter.position = position
+	$Button.rect_position = position - Vector2(36,38)
+
+func click():
+	if Input.is_action_just_pressed("mouse"):
+		if click_rect.has_point(get_global_mouse_position()):
+			sleeping = true
+			print("mouse click" + colors[number])
+			yield(get_tree().create_timer(0.5),"timeout")
+			queue_free()
 
 func set_start_position(pos : Vector2):
 	start_position = pos
 
+#func _input(event):
+#	if event is InputEventMouseButton:
+#		if event.is_pressed() and event.button_index == BUTTON_LEFT:
+#			if click_rect.has_point(get_global_mouse_position()):
+#				sleeping = true
+#				print("mouse click" + colors[number])
+#				yield(get_tree().create_timer(0.5),"timeout")
+#				queue_free()
+
 func launch():
 	angle = rand_range(min_angle, max_angle)
-	force = rand_range(min_force, max_force)
+	
+	var angle_range = max_angle - min_angle
+	var new_max = max_force - 90 * ((angle - min_angle) / angle_range)
+	force = rand_range(min_force, new_max)
 	
 	var new_angle = 180 * float(from_left) - (angle * pow(-1,float(!from_left)))
 	
 	apply_impulse(Vector2(1,0), polar2cartesian(force, deg2rad(-new_angle)))
-	prints(angle, new_angle)
 
 func _on_VisibilityNotifier2D_screen_exited():
-	#Reinicia la posicion del RigidBody
-	position = Vector2(-63,330)
+	queue_free()
+
+func _on_Timer_timeout():
+	launch()
+
+func _on_click_detecter_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == BUTTON_LEFT:
+			sleeping = true
+			print("mouse click" + colors[number])
+			yield(get_tree().create_timer(0.5),"timeout")
+			queue_free()
+
+
+func _on_Button_pressed():
 	sleeping = true
+	print("mouse click" + colors[number])
+	yield(get_tree().create_timer(0.5),"timeout")
+	queue_free()
--- Scripts/juego.gd
+++ Scripts/juego.gd
@@ -2,9 +2,26 @@ extends Node2D
 
 export(PackedScene) var frutas
 
+var idx = -1
+var max_numbers : int = 7
+var numbers : Array = []
+
 func _ready():
-	pass
+	randomize()
+	
+	for i in max_numbers:
+		numbers.append(i)
+	numbers.shuffle()
 
 func _on_fruta_spawner_timeout():
 	var f = frutas.instance()
+	idx = wrapi(idx + 1, 0, max_numbers)
+	f.number = numbers[idx]
 	
+	var left = randi() % 2
+	f.position = $spawn_point.position + Vector2.RIGHT * 1100 * left
+	f.from_left = bool(left)
+	add_child(f)
+	
+	if idx == max_numbers - 1:
+		numbers.shuffle()
